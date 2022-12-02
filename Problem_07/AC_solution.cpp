1670b2e39b9c321b5bf14e3c1dafb1bfb9f79dfa
Mahmoud-nfz <65515933+Mahmoud-nfz@users.noreply.github.com>
2022-12-02 20:55:51 +0100

    solution problem 7

--- Problem_07/AC_solution.cpp
+++ Problem_07/AC_solution.cpp
@@ -6,6 +6,19 @@ int main() {
     while(t--) {
 
         int n, m; cin >> n >> m;
+        int prev = 0 ;
+        int ans = 0 ;
+        for(int i = 0 ; i < n ; i ++){
+            string s ;
+            cin >> s ;
+            for(int j = 0 ; j < s.size(); j ++){
+                if(s[j] == '.'){
+                    ans += abs(j-prev) ;
+                    prev = j ;
+                }
+            }
+        }
+        cout << ans << endl ;
 
     }
     return 0;
