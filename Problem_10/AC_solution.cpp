99442d7e26da5bf74f6fb7082babb68cbc498307
Mohamed Ali Ouachani <36360218+IR0NBYTE@users.noreply.github.com>
2022-12-02 11:55:51 +0100

    Update AC_solution.cpp

--- Problem_10/AC_solution.cpp
+++ Problem_10/AC_solution.cpp
@@ -24,8 +24,7 @@ int a[2005];
 int tmp[2005];
 
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     //freopen("input.txt", "r", stdin);
     //freopen("output.txt", "w", stdout);
