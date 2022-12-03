5c1fb04e37c932711d893d7b86c2d5b15bb9b4a2
Idris Saddi <47497916+idris-saddi@users.noreply.github.com>
2022-12-03 17:56:25 +0100

    update ac_solution problem 1

--- Problem_01/AC_solution.cpp
+++ Problem_01/AC_solution.cpp
@@ -1,12 +1,22 @@
-#include <bits/stdc++.h>  
+#include <iostream>  
+
 using namespace std;
+string Filter(const string &to){
+    string final;
+    for(string::const_iterator it = to.begin(); it != to.end(); ++it)
+        if((*it >= '0' && *it <= '9'))
+            final += *it;
+    return final;
+}
 
 int main() {
     int t; cin >> t;
     while(t--) {
-
-        int n, m; cin >> n >> m;
-
+        string s;
+        cin>>s;
+        s = Filter(s);
+        if(s.size()==8&&(s[0]=='2'||s[0]=='5'||s[0]=='9')) cout<<s<<endl;
+        else cout<<-1<<endl;
     }
     return 0;
 }
