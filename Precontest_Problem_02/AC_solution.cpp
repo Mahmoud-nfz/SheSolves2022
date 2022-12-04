4ad61196f24f1c4e328099e9f040163a533e4623
Dhaouadi Yassine <91637898+YassDH@users.noreply.github.com>
2022-12-04 16:27:39 +0100

    Update AC_solution.cpp

--- Precontest_Problem_02/AC_solution.cpp
+++ Precontest_Problem_02/AC_solution.cpp
@@ -1,12 +1,31 @@
 #include <bits/stdc++.h>  
 using namespace std;
+#define ll long long
 
+ll somme (ll premierTerme,ll dernierTerme){
+    return (dernierTerme-premierTerme+1)*(premierTerme+dernierTerme)/2;
+}
 int main() {
     int t; cin >> t;
     while(t--) {
+        ll a0,an,sum,test,res;
+        cin >> a0 >> an; // READING THE INPUTS a0 > 0
+        sum = somme(a0,an); // CALCULATING THE SUM OF THE SEQUENCE (FROM a0 to an)
+        test = sum / an;
+        if((long double)sum/test <= an)
+            res = test-1;
+        else if((long double)sum/test > an+1)
+            res = test+1;
+        else
+            res = test;
 
-        int n, m; cin >> n >> m;
+        if( ((long double)sum/res <= an+1) && ((long double)sum/res > an) ){ // OUTPUTING THE RESULT
+            cout << res ;
+        }else{
+            cout << "NO";
+        }
 
+        cout << endl;
     }
     return 0;
 }
