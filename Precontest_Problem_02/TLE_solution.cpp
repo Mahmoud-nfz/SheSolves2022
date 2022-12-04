29c21b2cd07f1e067e5f0b642c8a280603e2d334
Dhaouadi Yassine <91637898+YassDH@users.noreply.github.com>
2022-12-04 18:01:21 +0100

    Update TLE_solution.cpp

--- Precontest_Problem_02/TLE_solution.cpp
+++ Precontest_Problem_02/TLE_solution.cpp
@@ -1,12 +1,30 @@
-#include <bits/stdc++.h>  
-using namespace std;
+    #include <bits/stdc++.h>
+    using namespace std;
+    #define ll long long
 
-int main() {
-    int t; cin >> t;
-    while(t--) {
-
-        int n, m; cin >> n >> m;
-
+    int main() {
+        int t; cin >> t;
+        while(t--) {
+            ll a0,an,sum=0,begin, end;
+            bool res = true;
+            cin >> a0 >> an; // READING THE INPUTS a0 > 0
+            for (ll i = a0; i < an+1 ; ++i) { // CALCULATING THE SUM OF THE SEQUENCE (FROM a0 to an) //HERE THE TLE
+                sum+=i;
+            }
+            begin = sum / an;
+            end = sum / (an+1);
+            for (ll i = begin; i < end+2; ++i) {// OUTPUTING THE RESULT IF IT EXISTS
+                if( ((long double)sum/i > (an+1)) ){
+                    break;
+                }else if( ((long double)sum/i > an) ){
+                    cout << i;
+                    res = false;
+                    break;
+                }
+            }
+            if(res)
+                cout << "NO";
+            cout << endl;
+        }
+        return 0;
     }
-    return 0;
-}
