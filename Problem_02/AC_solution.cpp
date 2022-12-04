f77bc7c32882789a0912ccb55c00ad054655467e
Mahmoud-nfz <65515933+Mahmoud-nfz@users.noreply.github.com>
2022-12-04 16:30:31 +0100

    Update AC_solution.cpp

--- Problem_02/AC_solution.cpp
+++ Problem_02/AC_solution.cpp
@@ -5,7 +5,17 @@ int main() {
     int t; cin >> t;
     while(t--) {
 
-        int n, m; cin >> n >> m;
+        int n, k; cin >> n >> k;
+        int a[n] ;
+        int sum = 0 ;
+        for(int i = 0; i < n; i ++){
+            cin >> a[i] ;
+            sum += a[i] ;
+        }
+        if(sum % k == 0)
+            cout << "YES\n" ;
+        else
+            cout << "NO\n" ;
 
     }
     return 0;
