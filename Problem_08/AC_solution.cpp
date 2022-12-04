809ee36e61521c9cbea923c4ec79d306dc15464e
Mahmoud-nfz <65515933+Mahmoud-nfz@users.noreply.github.com>
2022-12-04 03:09:40 +0100

    ac solution problem 8

--- Problem_08/AC_solution.cpp
+++ Problem_08/AC_solution.cpp
@@ -1,12 +1,33 @@
 #include <bits/stdc++.h>  
 using namespace std;
 
+
+void solve(){
+    int n ;
+    cin >> n ;
+    int a[n] ;
+    set<int> colors ;
+    for(int i = 0; i < n; i ++){
+        cin >> a[i] ;
+        if(a[i] != -1)
+            colors.insert(a[i]) ;
+    }
+    if(n == 1){
+        cout << 1 << endl ;
+        return ;
+    }
+    for(int i = 1; i < n; i ++){
+        if(a[i] == a[i-1] && a[i] != -1){
+            cout << -1 << endl ;
+            return ;
+        }
+    }
+    cout << max(2,(int)colors.size()) << endl ;
+}
 int main() {
     int t; cin >> t;
     while(t--) {
-
-        int n, m; cin >> n >> m;
-
+        solve() ;
     }
     return 0;
 }
