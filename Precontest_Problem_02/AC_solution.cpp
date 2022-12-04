bdf0244864317d6785fe55f7e111947a488282e4
Dhaouadi Yassine <91637898+YassDH@users.noreply.github.com>
2022-12-04 17:49:41 +0100

    Update AC_solution.cpp

--- Precontest_Problem_02/AC_solution.cpp
+++ Precontest_Problem_02/AC_solution.cpp
@@ -8,23 +8,24 @@ ll somme (ll premierTerme,ll dernierTerme){
 int main() {
     int t; cin >> t;
     while(t--) {
-        ll a0,an,sum,test,res;
+        ll a0,an,sum,begin, end;
+        bool res = true;
         cin >> a0 >> an; // READING THE INPUTS a0 > 0
         sum = somme(a0,an); // CALCULATING THE SUM OF THE SEQUENCE (FROM a0 to an)
-        test = sum / an;
-        if((long double)sum/test <= an)
-            res = test-1;
-        else if((long double)sum/test > an+1)
-            res = test+1;
-        else
-            res = test;
+        begin = sum / an;
+        end = sum / (an+1);
 
-        if( ((long double)sum/res <= an+1) && ((long double)sum/res > an) ){ // OUTPUTING THE RESULT
-            cout << res ;
-        }else{
-            cout << "NO";
+        cout << " sum :" << sum  << endl ;
+
+        for (ll i = begin; i < end+2; ++i) {
+            if( ((long double)sum/i <= (an+1)) && ((long double)sum/i > an) ){ // OUTPUTING THE RESULT IF IT EXISTS
+                cout << i;
+                res = false;
+                break;
+            }
         }
-
+        if(res)
+            cout << "NO";
         cout << endl;
     }
     return 0;
