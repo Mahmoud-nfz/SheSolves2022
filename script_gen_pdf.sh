b2bfdcbf940baa61c125035c81ca80039c723aa7
mahmoud nefzi <mahmoud.nefzi@vneuron.com>
2022-12-07 22:23:30 +0100

    pdf statements

--- script_gen_pdf.sh
+++ script_gen_pdf.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# if not installed markdown-pdf
+# sudo npm install -g markdown-pdf
 for i in {01..11}
 do
   markdown-pdf "Problem_$i/statement.md" -o "./pdf_statements/Problem_$i.pdf" ;
