9147efabd6360e223eb598494575ba0c7cd28686
Addala M. Raed <addala.raed@gmail.com>
2022-12-08 03:30:01 +0100

    Update AC_solution.cpp
    el commit elli 9belha ta3ti runtime error. Hethi jarebtha mchitli ema netsawer nzidou njarbouha m3a edge cases o5rin. Just to be sure.

--- Problem_11/AC_solution.cpp
+++ Problem_11/AC_solution.cpp
@@ -5,12 +5,12 @@ int main() {
     long long t,n,f,c,k,d; cin >> t;
     while(t--) {
         cin >> n >> f >> c >>k;
-        d = -1;
+        d = 1;
         for(long long i = n ; i > 0 ; i /=2)
         {
             while( ( (n+(d+i)*c-1)/((d+i)*c) ) * k > f) d+=i;
         }
-        cout << d << endl;
+        cout << d+1 << endl;
     }
     return 0;
 }
