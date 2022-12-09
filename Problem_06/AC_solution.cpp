80fb642d397f66bb3b76e42e14776c23e24e4e89
Addala M. Raed <addala.raed@gmail.com>
2022-12-09 18:14:52 +0100

    Update AC_solution.cpp

--- Problem_06/AC_solution.cpp
+++ Problem_06/AC_solution.cpp
@@ -1,12 +1,38 @@
 #include <bits/stdc++.h>  
 using namespace std;
-
+vector<vector<int>> connections = {
+    {0,3,5,6,8,9},
+    {1,7},
+    {2},
+    {0,3,5,6,8,9},
+    {4},
+    {0,3,5,6,8,9},
+    {0,3,5,6,8,9},
+    {1,7},
+    {0,3,5,6,8,9},
+    {0,3,5,6,8,9}
+};
 int main() {
-    int t; cin >> t;
+    int t,n;
+    bool ok;
+    cin >> t;
+    string s1,s2;
     while(t--) {
-
-        int n, m; cin >> n >> m;
-
+        cin >>n;
+        cin >>s1;
+        cin >>s2;
+        ok = true;
+        for(int i = 0 ; i < n ; ++i)
+        {
+            if( find(connections[s1[i]-'0'].begin(), connections[s1[i]-'0'].end(),s2[i]-'0') ==  connections[s1[i]-'0'].end())
+            {
+                cout << "NO" << endl;
+                ok = false;
+                break;
+            }
+        }
+        if(ok) cout << "YES" << endl;
     }
     return 0;
 }
+
