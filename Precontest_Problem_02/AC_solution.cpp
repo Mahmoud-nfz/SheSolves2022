977d41303a772da6edce69122b3b82fa8a4d7bc0
Dhaouadi Yassine <91637898+YassDH@users.noreply.github.com>
2022-12-09 23:44:25 +0100

    Update AC_solution.cpp

--- Precontest_Problem_02/AC_solution.cpp
+++ Precontest_Problem_02/AC_solution.cpp
@@ -2,29 +2,30 @@
 using namespace std;
 #define ll long long
 
-ll somme (ll premierTerme,ll dernierTerme){
-    return (dernierTerme-premierTerme+1)*(premierTerme+dernierTerme)/2;
-}
 int main() {
     int t; cin >> t;
     while(t--) {
-        ll a0,an,sum,begin, end;
-        bool res = true;
-        cin >> a0 >> an; // READING THE INPUTS a0 > 0
-        sum = somme(a0,an); // CALCULATING THE SUM OF THE SEQUENCE (FROM a0 to an)
-        begin = sum / an;
-        end = sum / (an+1);
-        for (ll i = begin; i < end+2; ++i) {// OUTPUTING THE RESULT IF IT EXISTS
-            if( ((long double)sum/i > (an+1)) ){
-                break;
-            }else if( ((long double)sum/i > an) ){
-                cout << i;
-                res = false;
+        ll size;
+        cin >> size;
+        ll sequence[size];
+        for (ll i = 0; i < size; ++i) {
+            cin >> sequence[i];
+        }
+        ll sum = sequence[0];
+        bool found = false;
+        for (ll k = 1; k < size-1; ++k) {
+            sum += sequence[k];
+            long double sumDivk = (long double)sum/k;
+            if((sequence[k] < sumDivk) && (sumDivk <= sequence[k+1])){
+                cout << k ;
+                found = true;
                 break;
             }
         }
-        if(res)
-            cout << "NO";
+        if(!found){
+            cout << -1;
+        }
+
         cout << endl;
     }
     return 0;
